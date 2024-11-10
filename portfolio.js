1a7ac132c0424ee57d72c067118e102abbc4603b
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-11-10 15:29:43 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// Task 2: Create the Portfolio Module
+
+import { assets } from './asset.js';
+
+export function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + asset.price * asset.quantity, 0);
+}
+
+export function getPortfolioAllocation() {
+    const totalValue = calculatePortfolioValue();
+    return assets.map(asset => ({
+        name: asset.name,
+        allocation: ((asset.price * asset.quantity) / totalValue) * 100
+    }));
+}
